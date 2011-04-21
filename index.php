c8cbecf783903b2c6c4d13675727d21c25de528f
Mark Headd <mheadd@voiceingov.org>
2011-04-21 16:51:28 -0500

    Added code comments, language options prompt.

--- index.php
+++ index.php
@@ -10,11 +10,23 @@ require_once 'classes/tropo.class.php';
 dispatch_post('start', 'tropoStart');
 function tropoStart() {
 	
+	// Create a new instance of the Tropo object.
 	$tropo = new Tropo();	
+	
+	// Welcome message, and get caller input.
 	$tropo->say("Welcome to the tropo orchestra example.", array("barge" => false));
-	$tropo->ask("What is your favoirte programming language?", array("attempts" => 3,"choices" => "PHP, Ruby, JavaScript, Python", "name" => "language", "timeout" => 5));
+	$tropo->ask("What is your favoirte programming language? You can say PHP, Ruby, Javascript or Python.", 
+				array("attempts" => 3,
+					  "choices" => "PHP, Ruby, JavaScript, Python", 
+					  "name" => "language", 
+					  "timeout" => 5)
+				);
+	
+	// Event handlers.
 	$tropo->on(array("event" => "continue", "next" => "index?uri=end", "say" => "Please hold."));
 	$tropo->on(array("event" => "error", "next" => "index?uri=error", "say" => "An error has occured."));
+	
+	// Render JSON for Tropo to consume.
 	$tropo->renderJSON();
 	
 }
@@ -23,11 +35,14 @@ function tropoStart() {
 dispatch_post('end', 'tropoEnd');
 function tropoEnd() {
 	
+	// Create a new instance of the result object, and get caller selection.
 	$result = new Result();
 	$selection = $result->getValue();	
 	
+	// Create a new instance of the Tropo object.
 	$tropo = new Tropo();
 	
+	// Alternate prompt based on caller selection.
 	switch($selection) {
 		
 		case 'PHP':
@@ -47,10 +62,15 @@ function tropoEnd() {
 		
 	}
 	
+	// Read out prompt based on caller langage selection.
 	$tropo->say("You chose, " . $selection);	
 	$tropo->say($toSay);	
 	$tropo->say("Thanks for playing along. Goodbye");	
-	$tropo->hangup();	
+	
+	// Hangup when done.
+	$tropo->hangup();
+		
+	// Render JSON for Tropo to consume.
 	$tropo->renderJSON();
 
 }
@@ -59,11 +79,19 @@ function tropoEnd() {
 dispatch_post('error', 'tropoError');
 function tropoError() {
 	
+	// Create a new instance of the Tropo object.
 	$tropo = new Tropo();	
-	$tropo->say("Sorry, an error has occured. Please try again later");	
-	$tropo->hangup();	
+	
+	// Error prompt.
+	$tropo->say("Sorry, an error has occured. Please try again later");
+
+	// Hangup when done.
+	$tropo->hangup();
+	
+	// Render JSON for Tropo to consume.
 	$tropo->renderJSON();
 	
 }
 
+// Run the app.
 run();
